243599fdccbc6be91173678e5c8d9a90a3191341
KhurramSagoo <uaf.khurram@gmail.com>
2024-01-06 10:13:30 +0500

    changes in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,14 @@
-import { Heading, Button, Center, Box, useToast } from "@chakra-ui/react";
+import {
+  Heading,
+  Button,
+  Center,
+  Box,
+  useToast,
+  Container,
+  Stack,
+  SimpleGrid,
+  GridItem,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebaseconfig/firebase";
@@ -7,6 +17,15 @@ import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 
 const Home = () => {
+  const somePostObject = {
+    title: "Sample Title",
+    user: "John Doe",
+    date: "2023-01-01",
+    views: 100,
+    detail: "This is the main content of the blog post.",
+    moreDetail: "Additional details about the blog post.",
+  };
+
   const navigate = useNavigate();
   const toast = useToast();
   const [user, setUser] = useState(null);
@@ -64,10 +83,28 @@ const Home = () => {
 
   return (
     <>
-      <Center w="100%" h="100vh" bg="#1A202C">
-        <Box bg="#CBD5E0" maxW="md" w="100%" padding="30px" margin="25px">
-          <Heading>You are at home page!</Heading>
-          {user ? <p>Welcome, {user.email}!</p> : <p>You are not logged in.</p>}
+      <Container w="100%">
+        <Box maxW="lg" w="100%" padding="30px" margin="25px">
+          <Stack>
+            <Heading>You are at home page!</Heading>
+            {user ? (
+              <p>Welcome, {user.email}!</p>
+            ) : (
+              <p>You are not logged in.</p>
+            )}
+          </Stack>
+
+          <SimpleGrid column={{ base: 1, md: 2, lg: 3 }}>
+            <GridItem py={10}>
+              {/* <SinglePost post={somePostObject} /> */}
+              {/* <CreatePost /> */}
+            </GridItem>
+          </SimpleGrid>
+
+          <SimpleGrid column={{ base: 1, md: 2, lg: 3 }}>
+            <GridItem rowGap={10}>{/* <GetPostDB /> */}</GridItem>
+          </SimpleGrid>
+
           <Button
             bg="#2D3748"
             color="white"
@@ -81,7 +118,7 @@ const Home = () => {
             Log out
           </Button>
         </Box>
-      </Center>
+      </Container>
     </>
   );
 };
